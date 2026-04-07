7df4c4fa38f3ee693b07c4f894136eab95e67478
JoelVargis <108501520+JoelVargis@users.noreply.github.com>
2026-04-07 11:37:44 +0530

    Add files via upload

---
+++ WCFServicePayment/App_Code/IService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService" in both code and config file together.
+[ServiceContract]
+public interface IService
+{
+    [OperationContract]
+    PaymentResponse MakePayment(PaymentRequest request);
+}
+
+[DataContract]
+public class PaymentRequest
+{
+    [DataMember]
+    public long AccountNo { get; set; }
+
+    [DataMember]
+    public int Amount { get; set; }
+
+    [DataMember]
+    public int BillId { get; set; }
+
+    [DataMember]
+    public int OrderId { get; set; }
+}
+
+[DataContract]
+public class PaymentResponse
+{
+    [DataMember]
+    public bool IsSuccess { get; set; }
+
+    [DataMember]
+    public string Message { get; set; }
+}
---
+++ WCFServicePayment/App_Code/Service.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using LogicLayer;
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
+
+public class Service : IService
+{
+    public PaymentResponse MakePayment(PaymentRequest request)
+    {
+        BankServiceReference.ServiceClient bankClient = null;
+
+        try
+        {
+            bankClient = new BankServiceReference.ServiceClient();
+
+            var bankResponse = bankClient.ProcessPayment(
+                request.AccountNo,
+                request.Amount
+            );
+
+            if (bankResponse.IsSuccess)
+            {
+                BillBLL billBll = new BillBLL();
+                OrdersBLL bll = new OrdersBLL();
+                billBll.MarkBillAsPaid(request.BillId);
+                bll.MarkOrderAsOrdered(request.OrderId);
+
+                return new PaymentResponse
+                {
+                    IsSuccess = true,
+                    Message = "Payment successful"
+                };
+            }          
+            return new PaymentResponse
+            {
+                IsSuccess = false,
+                Message = bankResponse.Message
+            };
+        }
+        catch
+        {
+            return new PaymentResponse
+            {
+                IsSuccess = false,
+                Message = "Payment service error"
+            };
+        }
+        finally
+        {
+            if (bankClient != null)
+            {
+                try { bankClient.Close(); }
+                catch { bankClient.Abort(); }
+            }
+        }
+    }
+}
